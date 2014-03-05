84b25dfff38236843959eda8bcdd61ba4e6570bc
OpenShift guest <bennux@gmail.com>
2014-03-04 19:11:47 -0500

    router created

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var http = require("http");
 var url = require("url"); 
 
-function start() {
+function start(route) {
 
     function onRequest(request, response) {
      var pathname = url.parse(request.url).pathname;
      console.log("Request for " + pathname + " received.");
+     
+     route(pathname);
+     
      response.writeHead(200, {"Content-Type": "text/plain"});
-     response.write("Hello world from benny at Work! Renton, WA (node.js on Cloud9 IDE)....server.js...added pathname");
+     response.write("Hello world from benny at Work! Renton, WA (node.js on Cloud9 IDE)....server.js...added pathname....added router");
      response.end();
 }
 
